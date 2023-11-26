da09adae1a65683cb7f88c19d7607094c5946871
mycahfrn <mikaelfernandezj.d@gmail.com>
2023-11-26 16:28:59 -0400

    Added: Python

---
+++ Python/Others/Print.py
@@ -0,0 +1,7 @@
+print("Hello Peter")
+
+name = 'Peter'
+
+print(f'Hello {name}')
+
+print(' Getting Recourses '.center(50, '-'))
---
+++ Python/Variables.py
@@ -0,0 +1,13 @@
+float = 3.14
+number = 3
+string = 'The wish'
+bool = True
+
+none = None
+
+print(type(float)) # Class Float
+print(type(number)) # Class Number
+print(type(string)) # Class String
+print(type(bool)) # Class Bool
+
+print(type(none)) # Class None
